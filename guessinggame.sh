206270d4aef3dec55123c4d2fdd76d7166ed1ca1
sachinjk11 <sachinjk11@gmail.com>
2021-01-03 11:42:42 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
-function guess(){
-    true_ans=$(ls -l |grep "^-"|wc -l)
-    while true;
-    do
-        echo "pleas enter your guess"
-        read  number
-        if [ $number -lt $true_ans ]
-        then
-            echo "your guess is Less then the true number"
-        elif [ $number -gt $true_ans ]
-        then
-            echo "your guess is Greater then the true number"
-        else
-            echo " congratulation,you are right!"
-        break;
-        fi
-    done
+function play_game {
+  # The answer must be the number of lines in the provided file.
+	answer=$(ls -l | wc -l)
+	let answer=$answer-1
+	correct=0
+  while [[ $correct -eq 0 ]]
+	do
+		echo "How many files are in this directory?"
+        	read response
+	        echo "You entered: $response"
+    # If the user-provided response is actually the answer, return a string that says "Congrats!"
+    # If it is less than the answer, say "That is too low"
+    # Otherwise, it must be larger than the answer, so say "That is too high"
+		if [[ $response -eq $answer ]]
+		then
+			echo "You are correct! Congratulations"
+			correct=1
+		elif [[ $response -lt $answer ]]
+		then
+			echo "That is too low, try again."
+		else
+			echo "That is too high, try again."
+		fi
+	done
 }
-echo "guess the files number in the current directory!"
-guess
+# Running the program
+echo "Start of the guessing game"
+play_game
+echo "End of program"
